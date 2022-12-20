1fc53769ae9cbdb96957ce9784ca334310ca3d97
Oleksandr Kostan <kostan.law@gmail.com>
2022-12-20 17:27:47 +0200

    btn

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -23,9 +23,9 @@ export const LinkStyled = styled(NavLink)`
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
   margin-top: 16px;
 
-  &.active {
+  /* &.active {
     color: black;
-  }
+  } */
 
   /* :hover:not(:active),
   :focus-visible:not(:active) {
